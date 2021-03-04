10e59e0593febb72808ca309db09ab1e39df5764
WebTech Bilişim <info@webtechbilisim.com.tr>
2021-03-04 21:17:54 +0300

    PaymentSource Value Added

--- enrol.php
+++ enrol.php
@@ -86,7 +86,7 @@ $request->setOrderId($OrderID . "-" . $USER->id . "-" . $course->id . "-" . $ins
 $request->setIpAddress($USER->lastip);
 $request->setPrice($cost);
 $request->setDescription('Moodle WeePay Payment Solution');
-//$request->setCallBackUrl($CFG->wwwroot . "/enrol/weepaypayment/callback.php");
+$request->setPaymentSource('MOODLE|WEBTECH_BILISIM|'.$CFG->version.'|'.$CFG->release);
 $request->setCallBackUrl($CFG->wwwroot . "/enrol/weepaypayment/callback.php?oid=".$OrderID . "-" . $USER->id . "-" . $course->id . "-" . $instance->id);
 $request->setPaymentGroup(\weepay\Model\PaymentGroup::PRODUCT);
 $request->setPaymentChannel(\weepay\Model\PaymentChannel::WEB);
